ddf2ac1f89a9f7ed1b150bf94eff0bfe041d266d
luan048 <luanmonteiropedrosa@gmail.com>
2024-08-01 23:13:22 -0300

    Few updates to finish

--- management-system/src/renderer/listPerformace/performace.jsx
+++ management-system/src/renderer/listPerformace/performace.jsx
@@ -12,10 +12,17 @@ const api = axios.create({
 function Performacemonth() {
 
     const [sales, setSales] = useState([])
+    const [products, setProducts] = useState([])
 
     useEffect(() => {
         api.get('/purchases')
             .then(response => setSales(response.data))
+            .catch(error => console.log(error))
+    }, [])
+
+    useEffect(() => {
+        api.get('/')
+            .then(response => setProducts(response.data))
             .catch(error => console.log(error))
     }, [])
 
@@ -38,24 +45,40 @@ function Performacemonth() {
     const bodyPerformace = (month, label) => {
         const filteredProducts = filterByMonth(sales, month)
 
-        const totalSalesMonth = filteredProducts.reduce((total, product) => {
+        const calculatePurchases = filteredProducts.reduce((total, product) => {
             const saleString = product.price ? product.price.replace('R$', '').replace('.', '').replace(',', '.').trim() : '0'
             const sale = parseFloat(saleString) || 0
-            const quantity = product.quantity
+            const quantity = parseFloat(product.quantity) || 0
             
-            return total + sale * quantity
+            return total + (sale * quantity)
         }, 0).toFixed(2)
 
+        const filteredPurchases = filterByMonth(products, month)
+
+        const totalSalesMonth = filteredPurchases.reduce((total, product) => {
+            const priceString = product.price ? product.price.replace('R$', '').replace('.', '').replace(',', '.').trim() : '0'
+            const price = parseFloat(priceString) || 0
+
+            return total + price
+        }, 0).toFixed(2)
+
+        const profitOfMonth = (parseFloat(totalSalesMonth) || 0) - (parseFloat(calculatePurchases) || 0)
+
         return (
-            <div className="section-title" key={month}>
+            <div className="section" key={month}>
                 <div className="div-title">
                     <i className="titles">{label}</i>
                 </div>
                 <div className="bodyGeneral">
                     <div className="elementsTable" style={{display : filteredProducts.length === 0 ? 'none' : 'block'}}>
-                        {totalSalesMonth}
-                        
+                        <p>Vendas no Mês: R$ {totalSalesMonth}</p>
+                        <p>Valor Total Gastos com Materiais: R$ {calculatePurchases}</p>
+                        <p>Lucro do Mês: R$ {profitOfMonth}</p>
                     </div>
+
+                    {filteredProducts.length === 0 && (
+                        <p className="mensg">Sem Métricas para esse Mês!</p>
+                    )}
                 </div>
             </div>
         )
@@ -65,9 +88,20 @@ function Performacemonth() {
     return (
         <header className="containerPerformace">
             <Siderbar fixed={true}/>
-
             <div className="bodyPerformace">
+                <article className="ghost"></article>
                 {bodyPerformace(1, 'Performace de Janeiro')}
+                {bodyPerformace(2, 'Performace de Fevereiro')}
+                {bodyPerformace(3, 'Performace de Março')}
+                {bodyPerformace(4, 'Performace de Abril')}
+                {bodyPerformace(5, 'Performace de Maio')}
+                {bodyPerformace(6, 'Performace de Junho')}
+                {bodyPerformace(7, 'Performace de Julho')}
+                {bodyPerformace(8, 'Performace de Agosto')}
+                {bodyPerformace(9, 'Performace de Setembro')}
+                {bodyPerformace(10, 'Performace de Outubro')}
+                {bodyPerformace(11, 'Performace de Novembro')}
+                {bodyPerformace(12, 'Performace de Dezembro')}
             </div>
         </header>
     )
